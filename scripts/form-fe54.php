5c56a4b51277bc58ded0fd82dcd747de8fda6e87
jeremy-radtke <77674577+jeremy-radtke@users.noreply.github.com>
2021-02-19 11:04:50 +0100

    Add files via upload

---
+++ scripts/form-fe54.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'Website',
+    'email_message' => 'Neue Anfrage',
+    'success_redirect' => '',
+    'email' => array(
+    'from' => 'radtke.jeremy@t-online.de',
+    'to' => 'radtke.jeremy@t-online.de'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor();
+    $processor->process($form);
+
+    ?>
